f2cd5349dd0abcb98c39de2d77c2c01ffbb1aa89
Marissa <32754349+mpagador@users.noreply.github.com>
2018-06-17 01:50:04 -0700

    added more messages, edited format of messages to followers, added txt file encoding

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 followers = tweepy.Cursor(api.followers).items()
-greetings = open("greetings.txt", "r")
+greetings = ["Hey", "Hello", "Hi"]
 
 
 #follows back all current followers
@@ -19,37 +19,31 @@ def follow_back():
 #writes the message, and directs the tweet at a random follower if the message type is an encouragement or compliment
 def message_writer(choice):
     if choice == 1:
-        positive = open("positive.txt", "r")
+        positive = open("positive.txt", "r", encoding="utf-8-sig")
         message = random.choice(positive.readlines())
     else:
         follower_list = list(followers)
         chosen_follower = random.choice(follower_list).screen_name
-        print(chosen_follower)
-        greeting = random.choice(greetings.readlines())
-        print(greeting)
+        greeting = random.choice(greetings)
         message = greeting + " @" + chosen_follower + ", "
-        print(message)
         if choice == 2:
-            encouragements = open("encouragements.txt", "r")
+            encouragements = open("encouragements.txt", "r", encoding="utf-8-sig")
             line = random.choice(encouragements.readlines())
-            print(line)
-            if greeting == "Hey":
-                message = message + line
-            elif greeting == "Hello":
-                message = message + "I want you to know that " + line
-            else:
-                message = message + "this is a reminder that " + line
-        else:
-            compliments = open("compliments.txt", "r")
-            line = random.choice(compliments.readlines())
-            print(line)
             if greeting == "Hey":
                 message = message + line
             elif greeting == "Hello":
                 message = message + "I just wanted to say that " + line
             else:
-                message = message + "I wanted to tell you that " + line
-    print(message)
+                message = message + "this is a reminder that " + line
+        else:
+            compliments = open("compliments.txt", "r", encoding="utf-8-sig")
+            line = random.choice(compliments.readlines())
+            if greeting == "Hey":
+                message = message + line
+            elif greeting == "Hello":
+                message = message + "I want you to remember that " + line
+            else:
+                message = message + "I just want to let you know that " + line
     return message
 
 
