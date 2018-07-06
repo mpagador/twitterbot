6db69a5795aa04a2bb3d55ba02afeebabb37c3e2
Marissa <32754349+mpagador@users.noreply.github.com>
2018-07-06 12:50:38 -0700

    small changes

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+#user = api.get_user('marissasbot')
 greetings = ["Hey", "Hello", "Hi"]
 follower_list = []
 
